a31ac5b2962eaa70b37853f76882dbb5d56c6b06
Kevin <hellokevinvogel@gmail.com>
2022-03-06 22:17:52 +0100

    chore(user): add entity

---
+++ src/api/user/user.entity.ts
@@ -0,0 +1,21 @@
+import { Exclude } from 'class-transformer';
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  public id!: number;
+
+  @Column({ type: 'varchar' })
+  public email!: string;
+
+  @Exclude()
+  @Column({ type: 'varchar' })
+  public password!: string;
+
+  @Column({ type: 'varchar', nullable: true })
+  public name: string | null;
+
+  @Column({ type: 'timestamp', nullable: true, default: null })
+  public lastLoginAt: Date | null;
+}
